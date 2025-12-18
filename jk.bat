99870451f0fef59e7b72a7703cdbbd0439a016f6
MGC55 <mariogc5555@gmail.com>
2025-12-18 18:24:13 -0500

    Add files via upload

---
+++ jk.bat
@@ -0,0 +1,15 @@
+start /max curl asciiquarium.live
+start curl ascii.live/donut
+start curl ascii.live/batman
+start curl ascii.live/can-you-hear-me
+start curl ascii.live/hes
+start curl ascii.live/knot
+start curl ascii.live/bnr
+start curl ascii.live/coin
+start curl ascii.live/parrot
+start curl ascii.live/dvd
+start curl ascii.live/playstation
+start curl ascii.live/nyan
+start curl ascii.live/torus-knot
+start curl asciiquarium.live
+start curl ascii.live/donut
---
+++ jk2.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set reps=5
+
+
+::start curl ascii.live/nyan (another option inside for)
+
+
+for /l %%i in (1, 1, %reps%) do (
+    start cmd /k "color a & curl ascii.live/nyan"
+)
+
+exit
